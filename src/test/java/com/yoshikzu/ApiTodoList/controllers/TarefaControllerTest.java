5c7d04776cc18cc1710576370103d775c0b1eeaa
yoshikzu <marcelo_yoshikazu@hotmail.com>
2023-11-17 22:38:42 -0300

    Criação de métodos da classe de teste TarefaController

--- src/test/java/com/yoshikzu/ApiTodoList/controllers/TarefaControllerTest.java
+++ src/test/java/com/yoshikzu/ApiTodoList/controllers/TarefaControllerTest.java
@@ -3,6 +3,8 @@ package com.yoshikzu.ApiTodoList.controllers;
 import com.yoshikzu.ApiTodoList.dtos.DadosTarefaDto;
 import com.yoshikzu.ApiTodoList.enums.Prioridade;
 import com.yoshikzu.ApiTodoList.models.Tarefa;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.springframework.http.MediaType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TarefaControllerTest {
 
     @Autowired
@@ -34,17 +37,15 @@ public class TarefaControllerTest {
 
     @Test
     @DisplayName("Deveria cadastrar tarefa e devolver codigo http 201")
-    void cadastrar_tarefa() throws Exception{
+    void cenario_a_cadastrar_tarefa() throws Exception{
         var dadosCadastro = new DadosTarefaDto("Tarefa","Descricao",null,false,Prioridade.ALTA,false,null);
 
-        var response = mvc
-                .perform(post("/tarefas")
+        var response = mvc.perform(post("/tarefas")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(dadosTarefaDtoJson.write(dadosCadastro).getJson()))
                 .andReturn().getResponse();
 
         var tarefa = new Tarefa(1L,"Tarefa","Descricao",null,false,Prioridade.ALTA,false,null);
-
         var jsonEsperado = dadosDetalhamentoTarefaJson.write(tarefa).getJson();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
@@ -53,7 +54,20 @@ public class TarefaControllerTest {
 
     @Test
     @DisplayName("Deveria devolver codigo http 400 pois tentou gravar com informacoes invalidas")
-    void cadastrar_Tarefa_invalida() throws Exception {
+    void cenario_b_cadastrar_tarefa_invalida() throws Exception {
+        var response = mvc
+                .perform(post("/tarefas"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deveria devolver código http 400, pois tentou gravar tarefa sem nome")
+    void cenario_c_cadastrar_tarefa_sem_nome() throws Exception{
+        var dadosCadastro = new DadosTarefaDto(null,"Descricao",null,false,Prioridade.ALTA,false,null);
+
         var response = mvc
                 .perform(post("/tarefas"))
                 .andReturn().getResponse();
@@ -63,6 +77,42 @@ public class TarefaControllerTest {
     }
 
 
+    @Test
+    @DisplayName("Deveria devolver código http 200 e retornar a tarefa cadastrada")
+    void cenario_d_visualizar_tarefa() throws Exception{
+        //var dadosCadastro = new DadosTarefaDto("Tarefa","Descricao",null,false,Prioridade.ALTA,false,null);
 
+        /*var response = mvc.perform(post("/tarefas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosTarefaDtoJson.write(dadosCadastro).getJson()))
+                .andReturn().getResponse();
+*/
+        var tarefa = new Tarefa(1L,"Tarefa","Descricao",null,false,Prioridade.ALTA,false,null);
+
+        var jsonEsperado = dadosDetalhamentoTarefaJson.write(tarefa).getJson();
+
+        var response = mvc
+                .perform(get("/tarefas/1"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
+    }
+
+    @Test
+    @DisplayName("Deveria retornar código 204 e excluir a tarefa")
+    void cenario_e_excluir_tarefa() throws Exception{
+        var dadosCadastro = new DadosTarefaDto("Tarefa","Descricao",null,false,Prioridade.ALTA,false,null);
+
+        var response = mvc.perform(post("/tarefas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosTarefaDtoJson.write(dadosCadastro).getJson()))
+                .andReturn().getResponse();
+
+        response = mvc.perform(delete("/tarefas/1"))
+                          .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
 
 }
